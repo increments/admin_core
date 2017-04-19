7e9d6c83fead68cb80f99442a7a19f22d645fdab
Yuku TAKAHASHI <taka84u9@gmail.com>
2017-04-19 21:51:18 +0900

    Remove unused codes

--- client/src/resource-field/BelongsTo.jsx
+++ client/src/resource-field/BelongsTo.jsx
@@ -5,26 +5,6 @@ import {Link} from "react-router-dom";
 import type {ResourceFieldView, ResourceField$BelongsTo} from "../decls";
 import {getValue} from "../resource-field-renderer";
 
-exports.getValue = function (field: ResourceField$BelongsTo) {
-  const fi = field.value.resource.fields.find(f => f.name === field.value.paramName);
-  if (fi) {
-    return getValue(fi);
-  }
-};
-
-exports.Index = exports.Show = function (field: ResourceField$BelongsTo) {
-  const resource = field.value.resource;
-  if (resource.showPath) {
-    return <Link to={resource.showPath}>{resource.displayName}</Link>;
-  } else {
-    return <span>{resource.displayName}</span>;
-  }
-};
-
-exports.New = exports.Edit = function(_field: ResourceField$BelongsTo, _onChange: (string, any) => void) {
-  throw new Error("Not implemented");
-};
-
 export default class BelongsTo implements ResourceFieldView {
   field: ResourceField$BelongsTo;
 
